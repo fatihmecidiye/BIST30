215a91a1c7236aa8c20abcc7eb35a51cd46d64ee
fatihmecidiye <fatih.mecidiye@boun.edu.tr>
2023-12-20 23:05:14 +0100

    show info in table

--- app/src/main/java/com/example/bist30monitor/StockAdapter.kt
+++ app/src/main/java/com/example/bist30monitor/StockAdapter.kt
@@ -6,7 +6,7 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class StockAdapter(private val stockList: MutableList<Stock>) :
+/*class StockAdapter(private val stockList: MutableList<Stock>) :
     RecyclerView.Adapter<StockAdapter.StockViewHolder>() {
 
     inner class StockViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
@@ -26,6 +26,37 @@ class StockAdapter(private val stockList: MutableList<Stock>) :
         holder.textViewStockName.text = "Hisse: ${currentStock.name}"
         holder.textViewStockPrice.text = "Fiyat: ${currentStock.price} ₺"
         holder.textViewStockDailyChange.text = "Günlük Değişim: ${currentStock.dailyChange}%"
+    }
+
+    override fun getItemCount(): Int {
+        return stockList.size
+    }
+
+    fun getStockList(): MutableList<Stock> {
+        return stockList
+    }
+}*/
+
+class StockAdapter(private val stockList: MutableList<Stock>) :
+    RecyclerView.Adapter<StockAdapter.StockViewHolder>() {
+
+    inner class StockViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val textViewStockName: TextView = itemView.findViewById(R.id.textViewStockName)
+        val textViewStockPrice: TextView = itemView.findViewById(R.id.textViewStockPrice)
+        val textViewStockDailyChange: TextView = itemView.findViewById(R.id.textViewStockDailyChange)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StockViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_stock_table_row, parent, false) // Use the updated table row layout
+        return StockViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: StockViewHolder, position: Int) {
+        val currentStock = stockList[position]
+        holder.textViewStockName.text = currentStock.name
+        holder.textViewStockPrice.text = "${currentStock.price} ₺"
+        holder.textViewStockDailyChange.text = "${currentStock.dailyChange}%"
     }
 
     override fun getItemCount(): Int {
